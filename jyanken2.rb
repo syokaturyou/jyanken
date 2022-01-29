86f98d5d2302d0b4ff6bddfd5c7adc23af2851f4
syokaturyou <teamsumagaku2@gmail.com>
2022-01-29 14:30:43 +0000

    じゃんけんアプリ途中

--- jyanken2.rb
+++ jyanken2.rb
@@ -7,7 +7,7 @@ puts "1(グー)2(チョキ)3(パー)4(戦わない)"
 myhand = gets.to_i
 hand = rand(1..3)
 
-
+for state in 1..99 do
   if type == 0
     if myhand == 1 && hand == 1
        puts "----------------"
@@ -79,36 +79,39 @@ hand = rand(1..3)
        puts "1~4のどれかを入力してください"
        type = 4
     end
+  end
   
-  elsif type == 1
+  if type == 1
     puts "あいこで.."
     puts "1(グー)2(チョキ)3(パー)"
     myhand = gets.to_i
     hand = rand(1..3)
     type = 0
+  end
     
-  # elsif type == 2
-  #   puts "あっちむいて.."
-  #   puts "1(上)2(下)3(左)4(右)"
-  #   mydirection = gets.to_i
-  # 　direction = rand(1..4)
-  # 　if mydirection == direction
-  #     puts "----------------"
-  #     puts "方向が合いました。貴方の勝ちです"
-  #     type = 4
-  #     puts "----------------"
-  #   else
-  #     puts "----------------"
-  #     puts "方向が合いませんでした。やり直しです"
-  #     type = 0
-  #     puts "----------------"
-  #     puts "じゃんけん..."
-  #     puts "1(グー)2(チョキ)3(パー)4(戦わない)"
-  #     myhand = gets.to_i
-  #     hand = rand(1..3)
-  #   end
+  if type == 2
+    puts "あっちむいて.."
+    puts "1(上)2(下)3(左)4(右)"
+    mydirection = gets.to_i
+  　direction = rand(1..4)
+  　if mydirection == direction
+      puts "----------------"
+      puts "方向が合いました。貴方の勝ちです"
+      type = 4
+      puts "----------------"
+    else
+      puts "----------------"
+      puts "方向が合いませんでした。やり直しです"
+      type = 0
+      puts "----------------"
+      puts "じゃんけん..."
+      puts "1(グー)2(チョキ)3(パー)4(戦わない)"
+      myhand = gets.to_i
+      hand = rand(1..3)
+    end
+  end
     
-  # elsif type == 3
+  # if type == 3
   #   puts "あっちむいて.."
   #   puts "1(上)2(下)3(左)4(右)"
   #   mydirection = gets.to_i
@@ -124,8 +127,5 @@ hand = rand(1..3)
   #     type = 0
   #     puts "----------------"
   #   end
+  # end
   
-  else
-  end
-  
-# end
