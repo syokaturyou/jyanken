fcc90f34249b4b5327ad82636fbbb9fbb4ba119d
syokaturyou <teamsumagaku2@gmail.com>
2022-01-29 13:05:36 +0000

    乱数設定

--- jyanken.rb
+++ jyanken.rb
@@ -1,22 +1,74 @@
 require "csv"
 puts "じゃんけん..."
-puts "0(グー)1(チョキ) 2(パー)3(戦わない)"
-jyanken_type = gets.to_i
-
-if jyanken_type == 0
+puts "1(グー)2(チョキ)3(パー)4(戦わない)"
+myhand = gets.to_i
+hand = rand(1..3)
+# puts hand
+if myhand == 1 && hand == 1
    puts "----------------"
    puts "ホイ!"
    puts 'あなた:グーを出しました'
-elsif jyanken_type == 1
+   puts '相手:グーを出しました'
+   puts "引き分けです"
+   puts "----------------"
+elsif myhand == 1 && hand == 2
+   puts "----------------"
+   puts "ホイ!"
+   puts 'あなた:グーを出しました'
+   puts '相手:チョキを出しました'
+   puts "貴方の勝ちです!"
+   puts "----------------"
+elsif myhand == 1 && hand == 3
+   puts "----------------"
+   puts "ホイ!"
+   puts 'あなた:グーを出しました'
+   puts '相手:パーを出しました'
+   puts "貴方の負けです"
+   puts "----------------"
+elsif myhand == 2 && hand == 1
    puts "----------------"
    puts "ホイ!"
    puts 'あなた:チョキを出しました'
-elsif jyanken_type == 2
+   puts '相手:グーを出しました'
+   puts "貴方の負けです"
+   puts "----------------"
+elsif myhand == 2 && hand == 2
+   puts "----------------"
+   puts "ホイ!"
+   puts 'あなた:チョキを出しました'
+   puts '相手:チョキを出しました'
+   puts "引き分けです"
+   puts "----------------"
+elsif myhand == 2 && hand == 3
+   puts "----------------"
+   puts "ホイ!"
+   puts 'あなた:チョキを出しました'
+   puts '相手:パーを出しました'
+   puts "貴方の勝ちです"
+   puts "----------------"
+elsif myhand == 3 && hand == 1
    puts "----------------"
    puts "ホイ!"
    puts 'あなた:パーを出しました'
-elsif jyanken_type == 3
+   puts '相手:グーを出しました'
+   puts "貴方の勝ちです"
+   puts "----------------"
+elsif myhand == 3 && hand == 2
+   puts "----------------"
+   puts "ホイ!"
+   puts 'あなた:パーを出しました'
+   puts '相手:チョキを出しました'
+   puts "貴方の負けです"
+   puts "----------------"
+elsif myhand == 3 && hand == 3
+   puts "----------------"
+   puts "ホイ!"
+   puts 'あなた:パーを出しました'
+   puts '相手:パーを出しました'
+   puts "引き分けです"
+   puts "----------------"
+elsif myhand == 4
    puts "じゃんけんを終了します"
 else
-   puts "0~3のどれかを入力してください"
+   puts "1~4のどれかを入力してください"
 end
