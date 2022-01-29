14c9344b8b40febe4523ff7242f025d3d4029af5
syokaturyou <teamsumagaku2@gmail.com>
2022-01-29 12:49:13 +0000

    じゃんけんアプリ作成開始

--- jyanken.rb
+++ jyanken.rb
@@ -1,56 +1,22 @@
 require "csv"
+puts "じゃんけん..."
 puts "0(グー)1(チョキ) 2(パー)3(戦わない)"
-memo_type = gets.to_i
-    
-if memo_type == 0
-   puts "拡張子を除いたファイル名を入力してください"
-   filename = gets.to_s
-   
-   if File.exist?(filename + '.csv')
-    puts '既に同名ファイルが存在します。'
-   else
-    puts "メモしたい内容を記入してください"
-    puts "完了したら Ctrl + D を押します"
-    naiyo = STDIN.read
-    File.write(filename + ".csv",naiyo)
-   end
+jyanken_type = gets.to_i
 
-elsif memo_type == 1
-   puts "拡張子を除いたファイル名を入力してください"
-   filename = gets.to_s
-   
-   if File.exist?(filename + '.csv')
-     puts "csvファイルの現内容を表示"
-     puts "----------------------"
-     f = File.open(filename + ".csv")
-     puts "#{f.read}"
-     puts "----------------------"
-     puts "更新内容を記入してください"
-     puts "完了したら Ctrl + D を押します"
-     naiyo = STDIN.read
-     File.write(filename + ".csv",naiyo)
-   else
-     puts '該当ファイルは存在しません。'
-   end
-   
-   elsif memo_type == 2
-   puts "拡張子を除いたファイル名を入力してください"
-   filename = gets.to_s
-   
-   if File.exist?(filename + '.csv')
-     puts "csvファイルの現内容を表示"
-     puts "----------------------"
-     f = File.open(filename + ".csv")
-     puts "#{f.read}"
-     puts "----------------------"
-     puts "更新内容を記入してください"
-     puts "完了したら Ctrl + D を押します"
-     naiyo = STDIN.read
-     File.write(filename + ".csv",naiyo)
-   else
-     puts '該当ファイルは存在しません。'
-   end
-   
+if jyanken_type == 0
+   puts "----------------"
+   puts "ホイ!"
+   puts 'あなた:グーを出しました'
+elsif jyanken_type == 1
+   puts "----------------"
+   puts "ホイ!"
+   puts 'あなた:チョキを出しました'
+elsif jyanken_type == 2
+   puts "----------------"
+   puts "ホイ!"
+   puts 'あなた:パーを出しました'
+elsif jyanken_type == 3
+   puts "じゃんけんを終了します"
 else
-   puts "1か2を入力してください"
+   puts "0~3のどれかを入力してください"
 end
